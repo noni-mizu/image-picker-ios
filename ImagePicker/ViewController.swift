89742f9d8cd3f742a107da20287719349e2a4f6b
Noni <noni_mizu@gmail.com>
2016-06-28 15:25:15 -0700

    Initial commit

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -8,18 +8,143 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var imgeView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName : UIColor.whiteColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : 5.0
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        // the notification center class adds an observer, which is a listener, waiting for notifications with the name UIKeyboardWillHideNotification. it also lists which function is listening for its broadcast, in this case: keyboardWillHide(_:)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        // assign delegates to text fields so the return key code will work
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // won't enable camera unless your phone has one
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        // Subscribe to keyboard notifications to allow the view to raise when necessary
+        subscribeToKeyboardNotifications()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // one of two optional methods from UIImagePickerControllerDelegate
+    func imagePickerController(picker: UIImagePickerController,
+                        didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        
+        // retreives image from the image picker, puts in your imagePickerView
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imgeView.image = image
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    // two of two optional methods from UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController){
+        
+        // dismisses the image picker
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Move the view up/down when the keyboard pops up over lower text field
+    func keyboardWillShow(notification: NSNotification) {
+        let userInfo: [NSObject : AnyObject] = notification.userInfo!
+        
+        let keyboardSize: CGSize = userInfo[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.size
+        let offset: CGSize = userInfo[UIKeyboardFrameEndUserInfoKey]!.CGRectValue.size
+        
+        if keyboardSize.height == offset.height {
+            if view.frame.origin.y == 0 {
+                UIView.animateWithDuration(0.1, animations: { () -> Void in
+                    self.view.frame.origin.y -= keyboardSize.height
+                })
+            }
+        } else {
+            UIView.animateWithDuration(0.1, animations: { () -> Void in
+                self.view.frame.origin.y += keyboardSize.height - offset.height
+            })
+        }
+    }
+    
+    // hides keyboard from notification
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            view.frame.origin.y += keyboardSize.height
+        }
+    }
+    
+    // configures keyboard hide, so can move view that amount
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
     }
 
+    // when press return in text field, keyboard dismisses
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    // picks image from camera
+    @IBAction func pickImage(sender: AnyObject) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    // picks a photo from photo album
+    @IBAction func pickAnImageFromAlbum (sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        // unsubscribes from the keyboard notification when app disappears
+        unsubscribeFromKeyboardNotifications()
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: self.view.window)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: self.view.window)
+    }
 
 }
 
